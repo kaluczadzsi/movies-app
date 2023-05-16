32cef8d6e05321935f5eb71816494c307fb1e373
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-05-16 19:01:11 +0200

    Aded hover on movies

--- script.js
+++ script.js
@@ -9,27 +9,42 @@ const SEARCHAPI =
   'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
 const main = document.querySelector('main');
+const search = document.querySelector('.search');
+const form = document.getElementById('form');
 
-async function getMovies() {
-  const resp = await fetch(APIURL);
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
 
-  respData.results.forEach((movie) => {
-    const { poster_path, title, vote_average } = movie;
+  showMovies(respData.results);
+}
+
+function showMovies(movies) {
+  // clear main
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
 
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
 
     movieEl.innerHTML = `
-    <img
-        src="${IMGPATH + poster_path}"
-        alt="${title}" 
-    />
-    <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-    </div>
-    `;
+  <img
+      src="${IMGPATH + poster_path}"
+      alt="${title}" 
+  />
+  <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(
+      2
+    )}</span>
+  </div>
+  <div class="overview">
+  ${overview}
+  </div>
+  `;
 
     main.appendChild(movieEl);
   });
@@ -45,4 +60,13 @@ function getClassByRate(vote) {
   }
 }
 
-console.log(getMovies());
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+    search.value = '';
+  }
+});
